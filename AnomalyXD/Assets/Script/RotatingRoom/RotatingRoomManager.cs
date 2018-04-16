b65d01b9d9ca0298af5e1a51a694df6594eabf6b
Bosko Ivkovic <bosko2003414@gmail.com>
2018-04-16 16:56:41 +0200

    RotatingRoomPuzzle

---
+++ AnomalyXD/Assets/Script/RotatingRoom/RotatingRoomManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingRoomManager : MonoBehaviour {
+
+	public int speed;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		
+	}
+}
