5e2ac03507809a64f26c7809623e58549e33c427
Bosko Ivkovic <bosko2003414@gmail.com>
2018-04-16 17:32:00 +0200

    RotationSceneAlmostfinished

--- AnomalyXD/Assets/Script/RotatingRoom/RotatingRoomManager.cs
+++ AnomalyXD/Assets/Script/RotatingRoom/RotatingRoomManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class RotatingRoomManager : MonoBehaviour {
 
-	public int speed;
+	public float rotationSpeed;
+	public int timeBeforeRotation;
+	public bool startRotating = false;
+	public GameObject spikes;
+
+	int randomizer;
 
 	void Start () 
 	{
-		
+		spikes.SetActive (false);
+		//dit moet op een trigger
+		StartCoroutine(RotationStart());
+		randomizer = Random.Range (1, 3);
 	}
 
 	void Update () 
 	{
-		
+		if (startRotating == true && randomizer == 1) 
+		{
+			transform.Rotate (new Vector3 (0, 0, rotationSpeed) * Time.deltaTime );
+		}
+
+		if (startRotating == true && randomizer == 2) 
+		{
+			transform.Rotate (new Vector3 (0, 0, -rotationSpeed) * Time.deltaTime );
+		}
 	}
+
+	IEnumerator RotationStart()
+	{
+		yield return new WaitForSeconds (timeBeforeRotation);
+		spikes.SetActive (true);
+		startRotating = true;
+	}
+
 }
