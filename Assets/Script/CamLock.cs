44c062e00f6ab127d13455ca4a8057224d07c202
rycon2424 <rycon2424@users.noreply.github.com>
2018-04-23 11:26:49 +0200

    Praise it works

--- Assets/Script/CamLock.cs
+++ Assets/Script/CamLock.cs
@@ -21,27 +21,25 @@ public class CamLock : NetworkBehaviour {
 		{
 			return;
 		}
-		character = this.transform.gameObject;
+		character = gameObject;
 		camera = this.gameObject.transform.GetChild (0).gameObject;
 	}
 
 	void Update () 
 	{
-		if (!isLocalPlayer)
+		if (isLocalPlayer)
 		{
-			return;
+			var md = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+
+			md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
+			smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f / smoothing);
+			smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
+			mouseLook += smoothV;
+			mouseLook.y = Mathf.Clamp(mouseLook.y, -60f, 60f);
+
+			camera.transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+			character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
 		}
-		var md = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
-
-		md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
-		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f / smoothing);
-		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
-		mouseLook += smoothV;
-		mouseLook.y = Mathf.Clamp(mouseLook.y, -60f, 60f);
-
-		camera.transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
-		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
-
 
         Ray myRay = new Ray(camera.transform.position, camera.transform.TransformDirection(Vector3.forward));
 
