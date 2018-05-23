d6c99041c87502d6780cf382939102d202916a3b
rycon2424 <rycon2424@users.noreply.github.com>
2018-05-23 11:01:09 +0200

    Update to 18

--- Assets/Script/NetworkingEnzo.cs
+++ Assets/Script/NetworkingEnzo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 public class NetworkingEnzo : NetworkManager {
@@ -8,6 +9,7 @@ public class NetworkingEnzo : NetworkManager {
 	private GameObject keeper;
 	private GameObject escapist;
 	public bool isKeeper = true;
+    public UnityEvent test;
 
 	public override void OnServerConnect(NetworkConnection conn)
 	{
@@ -16,13 +18,14 @@ public class NetworkingEnzo : NetworkManager {
 
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
 	{
-		if (isKeeper == true) {
+		if (isKeeper) {
 			keeper = Instantiate(Resources.Load("KEEPERBUTTHEREALONE"), new Vector3(2.4f, 1, 9), Quaternion.identity) as GameObject;
 
 			NetworkServer.AddPlayerForConnection(conn, keeper, playerControllerId);
+            test.Invoke();
 			isKeeper = false;
 		}
-		else if (isKeeper == false)
+		else if (!isKeeper)
 		{
 			escapist = Instantiate(Resources.Load("ESCAPISTBUTTHEREALONE 1"), new Vector3(7.7f, 1, -6.3f), Quaternion.identity) as GameObject;
 
